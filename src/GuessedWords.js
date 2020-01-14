2dfe98d8085b8d8da1b1afa08cf2896bb4aede1c
Afanasiy <Afanasiy@Irinas-MacBook-Pro.local>
2020-01-14 23:17:57 +0300

    Add GuessedWords component test

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,7 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const GuessedWords = props => {
-  return <div data-test="component-guessed-words"></div>;
+  return (
+    <div data-test="component-guessed-words">
+      {props.guessedWords.length === 0 ? (
+        <p data-test="guess-message">Guess a word</p>
+      ) : (
+        <div data-test="guessed-words">
+          <ul>
+            {props.guessedWords.map((word, i) => (
+              <li key={i} data-test="guessed-word">
+                {word.guessedWord} {word.letterMatchCount}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
 
 GuessedWords.propTypes = {
--- src/GuessesWords.test.js
+++ src/GuessesWords.test.js
@@ -11,12 +11,6 @@ const setup = (props = {}) => {
   return shallow(<GuessedWords {...setupProps} />);
 };
 
-test("renders without error", () => {
-  const wrapper = setup();
-  const component = findByTextAttr(wrapper, "component-guessed-words");
-  expect(component.length).toBe(1);
-});
-
 test("does not throw warning with expected props", () => {
   const expectedProps = {
     guessedWords: [
@@ -26,4 +20,49 @@ test("does not throw warning with expected props", () => {
   };
 
   checkProps(GuessedWords, expectedProps);
+});
+
+describe("if no words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+  test("renders without error", () => {
+    const component = findByTextAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  test("renders instructions to guess a word", () => {
+    const component = findByTextAttr(wrapper, "guess-message");
+    expect(component.text().length).not.toBe(0);
+  });
+});
+
+describe("if words are guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "brain", letterMatchCount: 2 },
+    { guessedWord: "party", letterMatchCount: 5 }
+  ];
+  beforeEach(() => {
+    wrapper = setup({
+      guessedWords
+    });
+  });
+
+  test("renders without error", () => {
+    const component = findByTextAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  test("renders 'Guessed words' section", () => {
+    const section = findByTextAttr(wrapper, "guessed-words");
+    expect(section.length).toBe(1);
+  });
+
+  test("renders correct number of guessed words", () => {
+    const section = findByTextAttr(wrapper, "guessed-word");
+    expect(section.length).toBe(guessedWords.length);
+  });
 });
