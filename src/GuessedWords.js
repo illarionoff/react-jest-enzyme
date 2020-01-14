b9e6530a4cb26130e86ae7f0f53d27745be1d3bd
Afanasiy <Afanasiy@Irinas-MacBook-Pro.local>
2020-01-14 22:46:39 +0300

    Add GuessedWords PropTypes testing

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -5,4 +5,13 @@ const GuessedWords = props => {
   return <div data-test="component-guessed-words"></div>;
 };
 
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
+
 export default GuessedWords;
--- src/GuessesWords.test.js
+++ src/GuessesWords.test.js
@@ -3,8 +3,9 @@ import { shallow } from "enzyme";
 import { findByTextAttr, checkProps } from "../test/testUtils";
 import GuessedWords from "./GuessedWords";
 
-const defaulProps = {};
-
+const defaulProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }]
+};
 const setup = (props = {}) => {
   const setupProps = { ...defaulProps, ...props };
   return shallow(<GuessedWords {...setupProps} />);
@@ -14,4 +15,15 @@ test("renders without error", () => {
   const wrapper = setup();
   const component = findByTextAttr(wrapper, "component-guessed-words");
   expect(component.length).toBe(1);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = {
+    guessedWords: [
+      { guessedWord: "train", letterMatchCount: 3 },
+      { guessedWord: "brain", letterMatchCount: 2 }
+    ]
+  };
+
+  checkProps(GuessedWords, expectedProps);
 });
